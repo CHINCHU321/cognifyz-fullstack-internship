66fc7eae924dc36a03d47a048bf479c95479a6c2
Chinchu Babu <chinchubabuofficial@gmail.com>
2024-05-17 08:19:38 +0400

    commit1

--- Level1/Task2/server.js
+++ Level1/Task2/server.js
@@ -4,9 +4,13 @@ const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname, '../index')));
 
 app.set('view engine', 'ejs');
+// Serve your index.html file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.post('/submit', (req, res) => {
     const { name, email, message } = req.body;
